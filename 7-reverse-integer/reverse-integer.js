0ea0579452f64a3ff028f5c50fa0695a1d24f819
Aditya Pandey <115188524+adityapandeydev@users.noreply.github.com>
2025-01-08 01:03:12 +0530

    Time: 83 ms (18.35%) | Memory: 53.1 MB (29.79%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+    let rev = 0;
+
+    while (x !== 0) {
+        const rem = x % 10;
+        rev = rev * 10 + rem;
+        x = (x / 10) | 0; // Remove fractional part, simulating integer division
+    }
+
+    if (rev < -(2 ** 31) || rev > 2 ** 31 - 1) {
+        return 0;
+    }
+
+    return rev;
+};
