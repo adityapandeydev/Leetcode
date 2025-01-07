9be2080251dc82285f65e1e25b276ad3f1458e37
Aditya Pandey <115188524+adityapandeydev@users.noreply.github.com>
2025-01-08 00:01:30 +0530

    Time: 3 ms (53.14%) | Memory: 8.5 MB (60.85%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.c
@@ -0,0 +1,18 @@
+#include <math.h>
+#include <limits.h>
+
+int reverse(int x) {
+    long rev = 0;
+
+    while (x != 0) {
+        int rem = x % 10;
+        rev = rev * 10 + rem;
+        x /= 10;
+    }
+
+    if (rev < INT_MIN || rev > INT_MAX) {
+        return 0;
+    }
+
+    return (int)rev;
+}
