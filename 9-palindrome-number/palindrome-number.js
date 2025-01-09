a2e0e980322dc8577f1a0442ecbc4c1dd4aaa19d
Aditya Pandey <115188524+adityapandeydev@users.noreply.github.com>
2025-01-09 22:47:57 +0530

    Time: 6 ms (80.47%) | Memory: 59 MB (40.73%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    if (x < 0) {
+        return false;
+    }
+
+    let original = x;
+    let rev = 0;
+
+    while (x > 0) {
+        rev = rev * 10 + (x % 10);
+        x = Math.floor(x / 10);
+    }
+
+    return rev === original;
+};
