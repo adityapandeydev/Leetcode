7259ca308268e4099b5f1feca1a3266d0f8bc6df
Aditya Pandey <115188524+adityapandeydev@users.noreply.github.com>
2025-01-13 23:51:38 +0530

    Time: 0 ms (100.00%) | Memory: 4 MB (56.63%) - LeetSync

---
+++ 27-remove-element/remove-element.go
@@ -0,0 +1,10 @@
+func removeElement(nums []int, val int) int {
+    p := 0
+    for i := 0; i < len(nums); i++ {
+        if nums[i] != val {
+            nums[p] = nums[i]
+            p++
+        }
+    }
+    return p
+}
