bda8d4aed9b1e3446746d5571c716d470073e930
Aditya Pandey <115188524+adityapandeydev@users.noreply.github.com>
2025-01-16 02:54:09 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (58.17%) - LeetSync

---
+++ 48-rotate-image/rotate-image.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for (int i = 0; i < n / 2; i++) {
+            int last = n - 1 - i;
+            for (int j = i; j < last; j++) {
+                int help = j - i;
+                int top = matrix[i][j];
+                matrix[i][j] = matrix[last - help][i];
+                matrix[last - help][i] = matrix[last][last - help];
+                matrix[last][last - help] = matrix[j][last];
+                matrix[j][last] = top;
+            }
+        }
+    }
+};
