6acb1c2603a65bb660808b4fed21e5a2cb045b73
Aditya Pandey <115188524+adityapandeydev@users.noreply.github.com>
2025-01-16 02:57:53 +0530

    Time: 0 ms (100.00%) | Memory: 51.6 MB (65.79%) - LeetSync

---
+++ 48-rotate-image/rotate-image.ts
@@ -0,0 +1,17 @@
+/**
+ Do not return anything, modify matrix in-place instead.
+ */
+function rotate(matrix: number[][]): void {
+    const n = matrix.length;
+    for (let i = 0; i < Math.floor(n / 2); i++) {
+        const last = n - 1 - i;
+        for (let j = i; j < last; j++) {
+            const help = j - i;
+            const top = matrix[i][j];
+            matrix[i][j] = matrix[last - help][i];
+            matrix[last - help][i] = matrix[last][last - help];
+            matrix[last][last - help] = matrix[j][last];
+            matrix[j][last] = top;
+        }
+    }
+}
