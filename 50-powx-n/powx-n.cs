ece5e3e4c10fe9cbb89983a132b4c4f55ec15015
Aditya Pandey <115188524+adityapandeydev@users.noreply.github.com>
2025-01-20 02:17:26 +0530

    Time: 0 ms (100.00%) | Memory: 29.1 MB (13.01%) - LeetSync

---
+++ 50-powx-n/powx-n.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public double MyPow(double x, int n) {
+        double ans = 1;
+        if (n > 0) {
+            while (n > 0) {
+                if (n % 2 != 0) {
+                    ans *= x;
+                    n--;
+                }
+                x *= x;
+                n = n >> 1;
+            }
+        } else {
+            while (n != 0) {
+                if (n % 2 != 0) {
+                    ans *= (1 / x);
+                    n++;
+                }
+                x *= x;
+                n = n >> 1;
+            }
+        }
+        return ans;
+    }
+}
