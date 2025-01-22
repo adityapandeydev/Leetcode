9b708d242cf7516866ce8295bd3f0221154919b0
Aditya Pandey <115188524+adityapandeydev@users.noreply.github.com>
2025-01-22 18:42:13 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (78.50%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -4,8 +4,7 @@ class Solution {
         int len = 0;
         for (int i = s.length() - 1; i >= 0; i--) {
             if (s.charAt(i) == ' ') {
-                len = s.substring(i, s.length() - 1).length();
-                return len;
+                return s.substring(i, s.length() - 1).length();
             }
         }
         return s.length();
