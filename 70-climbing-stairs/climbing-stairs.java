0659b43985dd9e0888bff8e32e7135d87fb2d865
Aditya Pandey <115188524+adityapandeydev@users.noreply.github.com>
2025-01-25 05:02:30 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (39.09%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,9 +1,30 @@
 class Solution {
     public int climbStairs(int n) {
-        double sqrt5 = Math.sqrt(5);
-        double phi = (1 + sqrt5) / 2;
-        double psi = (1 - sqrt5) / 2;
+        if (n <= 2) {
+            return n;
+        }
 
-        return (int) Math.round((Math.pow(phi, n + 1) - Math.pow(psi, n + 1)) / sqrt5);
+        int[][] matrix = {{1, 1}, {1, 0}};
+        int[][] result = matrixPower(matrix, n - 2); 
+        return result[0][0] * 2 + result[0][1];
+    }
+
+    private int[][] matrixPower(int[][] matrix, int n) {
+        int[][] result = {{1, 0}, {0, 1}}; 
+        while (n > 0) {
+            if (n % 2 == 1) {
+                result = multiplyMatrices(result, matrix);
+            }
+            matrix = multiplyMatrices(matrix, matrix);
+            n /= 2;
+        }
+        return result;
+    }
+
+    private int[][] multiplyMatrices(int[][] a, int[][] b) {
+        return new int[][]{
+            {a[0][0] * b[0][0] + a[0][1] * b[1][0], a[0][0] * b[0][1] + a[0][1] * b[1][1]},
+            {a[1][0] * b[0][0] + a[1][1] * b[1][0], a[1][0] * b[0][1] + a[1][1] * b[1][1]}
+        };
     }
 }
