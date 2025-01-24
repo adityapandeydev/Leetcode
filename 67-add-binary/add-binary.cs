39f42af0c4791a1bf8af719813c2bdfebbf7e087
Aditya Pandey <115188524+adityapandeydev@users.noreply.github.com>
2025-01-24 15:29:06 +0530

    Time: 1 ms (92.84%) | Memory: 41.4 MB (77.52%) - LeetSync

---
+++ 67-add-binary/add-binary.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public string AddBinary(string a, string b) {
+        StringBuilder sb = new StringBuilder();
+        int carry = 0;
+        int i = a.Length - 1;
+        int j = b.Length - 1;
+
+        while (i >= 0 || j >= 0 || carry == 1) {
+            if (i >= 0)
+                carry += a[i--] - '0';
+            if (j >= 0)
+                carry += b[j--] - '0';
+            sb.Append(carry % 2);
+            carry /= 2;
+        }
+        char[] result = sb.ToString().ToCharArray();
+        Array.Reverse(result);
+        return new string(result);
+    }
+}
