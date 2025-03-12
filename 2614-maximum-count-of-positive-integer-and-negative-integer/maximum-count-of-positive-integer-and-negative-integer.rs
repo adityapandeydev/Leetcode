e2f1a679e5a2731c1912465e64f8462cfaa51d1c
Aditya Pandey <adityapandey.inbox@gmail.com>
2025-03-12 19:59:38 +0530

    Time: 0 ms (100.00%) | Memory: 2.3 MB (100.00%) - LeetSync

---
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.rs
@@ -0,0 +1,18 @@
+impl Solution {
+    pub fn maximum_count(nums: Vec<i32>) -> i32 {
+        let mut neg_count = 0;
+        let mut i = 0;
+
+        while i < nums.len() {
+            if nums[i] < 0 {
+                neg_count += 1;
+            }
+            if nums[i] > 0 {
+                break;
+            }
+            i += 1;
+        }
+
+        std::cmp::max(neg_count, (nums.len() - i) as i32)
+    }
+}
